fe916bdb0cd907b18dcc19f2907c7b744345412e
Tim <privacywonk@privacywonk.net>
2019-08-31 08:55:41 -0400

    Added six new sources for blacklisting domains

--- void-zones-update.sh
+++ void-zones-update.sh
@@ -26,6 +26,15 @@
 # DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
 # IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Updated by Privacywonk 2019-08-31 to include six new sources for blacklists including:
+# https://github.com/StevenBlack/hosts
+# http://sysctl.org/cameleon/
+# Abuse.ch/
+# Disconnect.me trackers
+# Host-file ad servers
+# This should produce a de-duplicate blocking list of over 67,500 domains as of 2018-08-31
 
 
 ### verify the path to the fetch utility
@@ -47,6 +56,8 @@ fi
 ZONES_DIR="/usr/local/etc/void-zones"
 if [ ! -d "$ZONES_DIR" ]; then
    mkdir -p "$ZONES_DIR"
+fi
+if [ ! -f "$ZONES_DIR/my_void_hosts.txt" ]; then
    echo "# white list"          > "$ZONES_DIR/my_void_hosts.txt"
    echo "1.1.1.1 my.white.dom" >> "$ZONES_DIR/my_void_hosts.txt"
    echo ""                     >> "$ZONES_DIR/my_void_hosts.txt"
@@ -64,6 +75,14 @@ $FETCH -o "$ZONES_DIR/away_void_hosts.txt"     "https://adaway.org/hosts.txt"
 $FETCH -o "$ZONES_DIR/jdom_void_list.txt"      "http://mirror1.malwaredomains.com/files/justdomains"
 $FETCH -o "$ZONES_DIR/ucky_void_host.txt"     "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
 $FETCH -o "$ZONES_DIR/wintelm_void_hosts.txt" "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+#Added by PrivacyWonk
+$FETCH -o "$ZONES_DIR/stevenblack_void_hosts.txt" "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+$FETCH -o "$ZONES_DIR/sysctl_void_hosts.txt" "http://sysctl.org/cameleon/hosts"
+$FETCH -o "$ZONES_DIR/zeustracker_void_hosts.txt" "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist"
+$FETCH -o "$ZONES_DIR/disconnectmeTracking_void_hosts.txt" "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+$FETCH -o "$ZONES_DIR/disconnectmeAd_void_hosts.txt" "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+$FETCH -o "$ZONES_DIR/hostfileAd_hosts.txt" "https://hosts-file.net/ad_servers.txt"
+
 
 if [ ! -f "$ZONES_DIR/pgl_void_hosts.txt" ] ; then
    echo "# No hosts from pgl." > "$ZONES_DIR/pgl_void_hosts.txt"
@@ -97,6 +116,28 @@ if [ ! -f "$ZONES_DIR/wintelm_void_hosts.txt" ] ; then
    echo "# No hosts from WindowsSpyBlocker/hosts/spy." > "$ZONES_DIR/wintelm_void_hosts.txt"
 fi
 
+#PrivacyWonk Added
+if [ ! -f "$ZONES_DIR/stevenblack_void_hosts.txt" ] ; then
+   echo "# No hosts from Steven Black." > "$ZONES_DIR/stevenblack_void_hosts.txt"
+fi
+if [ ! -f "$ZONES_DIR/sysctl_void_hosts.txt" ] ; then
+   echo "# No hosts from sysctl." > "$ZONES_DIR/sysctl_void_hosts.txt"
+fi
+if [ ! -f "$ZONES_DIR/zeustracker_void_hosts.txt" ] ; then
+   echo "# No hosts from zeus tracker." > "$ZONES_DIR/zeustracker_void_hosts.txt"
+fi
+if [ ! -f "$ZONES_DIR/disconnectmeTracking_void_hosts.txt" ] ; then
+   echo "# No hosts from disconnectme tracking." > "$ZONES_DIR/disconnectmeTracking_void_hosts.txt"
+fi
+
+if [ ! -f "$ZONES_DIR/disconnectmeAd_void_hosts.txt" ] ; then
+   echo "# No hosts from disconnectme ad." > "$ZONES_DIR/disconnectmeAd_void_hosts.txt"
+fi
+if [ ! -f "$ZONES_DIR/hostfileAd_hosts.txt" ] ; then
+   echo "# No hosts from https://hosts-file.net/ad_servers.txt." > "$ZONES_DIR/hostfileAd_hosts.txt"
+fi
+
+
 /usr/local/bin/hosts2zones /tmp/local-void.zones \
                            "$ZONES_DIR/my_void_hosts.txt" \
                            "$ZONES_DIR/pgl_void_hosts.txt" \
@@ -110,4 +151,10 @@ fi
                            "$ZONES_DIR/x_void_list.txt" \
                            "$ZONES_DIR/y_void_list.txt" \
                            "$ZONES_DIR/z_void_list.txt" \
+							"$ZONES_DIR/stevenblack_void_hosts.txt" \
+							"$ZONES_DIR/sysctl_void_hosts.txt" \
+							"$ZONES_DIR/zeustracker_void_hosts.txt" \
+							"$ZONES_DIR/disconnectmeTracking_void_hosts.txt" \
+							"$ZONES_DIR/disconnectmeAd_void_hosts.txt" \
+							"$ZONES_DIR/hostfileAd_hosts.txt" \
   && /bin/mv /tmp/local-void.zones /var/unbound/local-void.zones
